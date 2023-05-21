eab67a459b134c0122bd779e02ffbf38c1cdbbce
Home <Home@DESKTOP-AFQ03O0>
2023-05-21 18:13:20 +0530

    changes3

--- src/test/java/utilities/PropertyFileUtil.java
+++ src/test/java/utilities/PropertyFileUtil.java
@@ -1,7 +1,9 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
-import java.io.FileOutputStream;
 
 public class PropertyFileUtil
 {
