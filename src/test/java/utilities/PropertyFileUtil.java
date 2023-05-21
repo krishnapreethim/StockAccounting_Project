7c350f8f1a37207e04d03e2300dd3bcf0ac26eec
Home <Home@DESKTOP-AFQ03O0>
2023-05-21 18:11:44 +0530

    changes2

--- src/test/java/utilities/PropertyFileUtil.java
+++ src/test/java/utilities/PropertyFileUtil.java
@@ -1,5 +1,17 @@
 package utilities;
 
-public class PropertyFileUtil {
+import java.util.Properties;
+import java.io.FileOutputStream;
+
+public class PropertyFileUtil
+{
+	public static String getValueForKey(String key) throws Exception
+	{
+	    Properties conprop = new Properties();
+		conprop.load(new FileInputStream("./PropertyFiles/Environment.properties"));
+		return conprop.getProperty(key);
+		}
+
+	
 
 }
